3bba89d3ef5ce35b230642d8d4c6fd89fff0500e
cofdream <cofdream@sina.com>
2020-11-18 16:23:49 +0800

    update

--- Project/Assets/Scripts/Archives/Archive.cs
+++ Project/Assets/Scripts/Archives/Archive.cs
@@ -7,9 +7,62 @@ using UnityEngine;
 public class Archive
 {
     public string Name = null;
-    public int lv = 0;
+
+    public Vector3 Position;
+    public Vector3 Rotation;
+
     public void Init()
     {
 
     }
+}
+
+public class Bag
+{
+    public Pet[] Pets;
+}
+
+public class Pet
+{
+    public int Id;
+
+    public int Lv;
+
+    public int Hp;
+    public int MaxHp;
+
+    public int WG;
+    public int TG;
+    public int WF;
+    public int TF;
+    public int S;
+
+    /// <summary>
+    /// 病毒
+    /// </summary>
+    public bool VirusStatus;
+
+    public bool EggStatus;
+
+    public int BallId;
+
+    public int PetSex;
+
+    public int[] EggGroup;
+
+    // 缎带
+}
+
+public enum PetSex
+{
+    None = 0,
+    Male,
+    Female,
+}
+
+public enum EggGroup
+{
+    None = 0,
+    Water,
+    Land,
 }
--- Project/Assets/Scripts/Archives/DataManager.cs
+++ Project/Assets/Scripts/Archives/DataManager.cs
@@ -5,20 +5,14 @@ namespace DA.DataModule
     public static class DataManager
     {
         public static DataBind<string> NameBind;
-        public static DataBind<int> LvBind;
         public static void Init()
         {
             NameBind = new DataBind<string>(SetName);
-            LvBind = new DataBind<int>(SetLv);
         }
 
         private static void SetName(string old, string @new)
         {
             ArchivesData.Archive.Name = @new;
-        }
-        private static void SetLv(int old, int @new)
-        {
-            ArchivesData.Archive.lv = @new;
         }
     }
 }
