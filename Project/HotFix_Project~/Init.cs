30e1fc0d0312810957a6cfe46d6b4b9e030adb60
cofdream <cofdream@sina.com>
2020-11-26 17:47:49 +0800

    update

---
+++ Project/HotFix_Project~/Init.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HotFix_Project
+{
+    public class Init
+    {
+        public void Func()
+        {
+            Debug.Log("Hello");
+        }
+        public void Func(int a)
+        {
+            Debug.Log(a);
+        }
+    }
+}
