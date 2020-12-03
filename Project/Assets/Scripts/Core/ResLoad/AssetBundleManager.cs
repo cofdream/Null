de8eb701cfa51b5069a12f1602d78aa9d1d9dd6a
cofdream <cofdream@sina.com>
2020-12-03 17:46:02 +0800

    update

---
+++ Project/Assets/Scripts/Core/ResLoad/AssetBundleManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DA.Res
+{
+    public static class AssetBundleManager
+    {
+        public enum AssetBundleLoadMode : byte
+        {
+            Develop = 0,
+            Simulation = 1,
+            AssetBundle = 2,
+        }
+        public static AssetBundleLoadMode loadMode = AssetBundleLoadMode.Develop;
+
+        static AssetBundleManager()
+        {
+
+        }
+
+        public static void Load<T>(string url, Action<T, bool> callBack)
+        {
+            
+        }
+    }
+}
