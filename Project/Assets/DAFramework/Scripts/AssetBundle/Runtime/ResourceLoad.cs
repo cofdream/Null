37c4cfd330784cf6320119ec57af3e5ad5dc17ac
cofdream <cofdream@sina.com>
2020-12-11 17:49:26 +0800

    update

---
+++ Project/Assets/DAFramework/Scripts/AssetBundle/Runtime/ResourceLoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DA.AssetsBundle
+{
+    public class Resource
+    {
+        public string Name { get => Data.name; }
+        public UnityEngine.Object Data { get; private set; }
+
+        
+    }
+
+    public class ResourceLoad
+    {
+
+    }
+}
---
+++ Project/Assets/DAFramework/Scripts/Editor/NewEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace NullNamespace
+{
+    public class NewEditor : EditorWindow
+    {
+        public static GameObject A;
+        public static GameObject B;
+
+        [MenuItem("TestLoad/Load A Assets")]
+        public static void AA()
+        {
+
+        }
+
+        [MenuItem("TestLoad/Load B Assets")]
+        public static void BB()
+        {
+
+
+        }
+
+        [MenuItem("TestLoad/Load Unload Assets")]
+        public static void Unload()
+        {
+
+        }
+
+        [MenuItem("TestLoad/Debug")]
+        public static void Debug()
+        {
+
+        }
+    }
+}
