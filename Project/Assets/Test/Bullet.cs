8494e55035f6c6f576ed556934e69242a8143c25
cofdream <cofdream@sina.com>
2021-01-21 16:33:34 +0800

    update

---
+++ Project/Assets/Test/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private bool fire = false;
+    private Vector3 direction;
+    private float speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fire)
+        {
+            transform.Translate(direction * Time.deltaTime * speed);
+        }
+    }
+
+    public void Fire(Vector3 position, Vector3 direction, float speed, Action<Bullet> action)
+    {
+        transform.position = position;
+        this.direction = direction;
+        this.speed = speed;
+        fire = true;
+
+        DA.Timer.Timer.StartTimer(new DA.Timer.TimerDisposable(3, () =>
+        {
+            fire = false;
+            action(this);
+        }));
+    }
+}
---
+++ Project/Assets/Test/Muzzle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+    public GameObject bulletGameObject;
+    public DA.ObjectPool.ObjectPool<Bullet> bulletPool;
+    void Start()
+    {
+        bulletPool = new DA.ObjectPool.ObjectPool<Bullet>();
+        bulletPool.Initialize(OnCreateBullet, OnDestoryBullet);
+    }
+
+    float recoilX = 0f;
+    float recoilY = 1f;
+
+    public float currentRecoil = 0;
+    float maxRecoil = 0.4f;
+    float addRecoil = 0.01f;
+    float remRecoil = -0.03f;
+
+    float lastFireTime = 0;
+    float resetFireIntervalTime = 0.35f;
+
+    public float intervalTime = 0.15f;
+    float curTime = 0;
+
+    void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && curTime > intervalTime)
+        {
+            curTime = 0;
+
+            float currentfireIntervalTime = Time.time - lastFireTime;
+            float intervalTime = currentfireIntervalTime - resetFireIntervalTime;
+            if (intervalTime > 0)
+            {
+                currentRecoil -= intervalTime * -10 * remRecoil;
+                if (currentRecoil < 0)
+                {
+                    currentRecoil = 0;
+                }
+            }
+            else
+            {
+                currentRecoil += intervalTime * -10 * addRecoil;
+
+                if (currentRecoil > maxRecoil)
+                {
+                    currentRecoil = maxRecoil;
+                }
+            }
+
+            lastFireTime = Time.time;
+
+            Vector3 position = transform.position;
+            Vector3 direction = transform.forward + new Vector3(recoilX * currentRecoil, recoilY * currentRecoil, 0);
+
+            var bullet = bulletPool.Allocate();
+            bullet.Fire(position, direction, 3f, (bulletArg) => bulletPool.Release(bulletArg));
+
+        }
+    }
+
+    private Bullet OnCreateBullet()
+    {
+        var _bulletGameObject = Instantiate(bulletGameObject);
+        return _bulletGameObject.GetComponent<Bullet>();
+    }
+    private void OnDestoryBullet(Bullet bullet)
+    {
+        Destroy(bullet.gameObject);
+    }
+
+}
