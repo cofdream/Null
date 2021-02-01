ff7d82af2797b10955a1b928fd2c005fdb7e06ad
cofdream <cofdream@sina.com>
2021-02-01 17:02:05 +0800

    update

---
+++ Project/Assets/Game/Scripts/Incubator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Incubator
+    {
+        private Vector3 bornPosition;
+        private Quaternion bornRotation;
+
+        private Soldier soldier;
+
+        private DA.Timer.TimerDisposable timer;
+
+        public Incubator(Vector3 bornPosition, Quaternion bornRotation, Soldier soldier)
+        {
+            this.bornPosition = bornPosition;
+            this.bornRotation = bornRotation;
+            this.soldier = soldier;
+
+            timer = new DA.Timer.TimerDisposable(1f, Update);
+        }
+
+        public void Update()
+        {
+            timer.Update(Time.deltaTime);
+        }
+    }
+}
---
+++ Project/Assets/Game/Scripts/Soldier.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Soldier
+    {
+        public GameObject soldierPrefab;
+
+        public void Create(Vector3 position, Quaternion rotation, Transform parent)
+        {
+            GameObject soldierGameObject = GameObject.Instantiate<GameObject>(soldierPrefab, position, rotation, parent);
+        }
+    }
+}
