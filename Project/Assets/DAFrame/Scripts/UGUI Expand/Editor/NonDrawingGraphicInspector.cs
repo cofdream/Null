985c999d22a0d0bf7d253b25a1261c4df6be1644
cofdream <cofdream@sina.com>
2021-03-05 14:39:11 +0800

    add ugui 扩展

---
+++ Project/Assets/DAFrame/Scripts/UGUI Expand/Editor/NonDrawingGraphicInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace NullNamespace
+{
+    [CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+    internal sealed class NonDrawingGraphicInspector : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(m_Script);
+
+            RaycastControlsGUI();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
