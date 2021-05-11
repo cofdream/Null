13a6549f29d2b98aa0f69d8edd03a40cd29cb9c2
cofdream <cofdream@sina.com>
2021-05-11 21:16:54 +0800

    移除list 优化，待后续修改方案

--- Project/Assets/DA/Scripts/Core/Timer/TimerManager.cs
+++ Project/Assets/DA/Scripts/Core/Timer/TimerManager.cs
@@ -2,14 +2,15 @@
 
 namespace DA.Timer
 {
+    // 对List 的优化停止
     public static class TimerManager
     {
         private static List<ITimer> timers;
-        private static int timerCount;
+        //private static int timerCount;
 
         private static int minCapacity = 20;
-        private static float disposeCurrentTime;    // 释放时间计时 单位：s
-        private static float disposeTotalTime = 60; // 释放时间     单位：s
+        //private static float disposeCurrentTime;    // 释放时间计时 单位：s
+        //private static float disposeTotalTime = 60; // 释放时间     单位：s
         public static bool TimerState { get; set; }
         static TimerManager()
         {
@@ -24,57 +25,74 @@ namespace DA.Timer
 
             var delta = FrameUpdater.DeltaTime;
 
-            int length = timerCount;
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < timers.Count; i++)
             {
                 var timer = timers[i];
                 bool isRemove = timer.Update(delta);
                 if (isRemove)
                 {
                     timer.Dispose();
-                    length--;
-                    timers[i] = timers[length];
-
-                    timers[length] = null;
-                    //timers.RemoveAt(length);
+                    timers[i] = null;
+                    timers.RemoveAt(i);
                 }
                 else
                 {
-                    timers[i] = timer;
                 }
             }
 
+            // 可能不对，暂时不释放
+
             // 在指定释放时间内，检测池大小是否过大。释放一半数组
-            int capacity = timers.Capacity;
-            if (length < capacity * 0.25f)
-            {
-                disposeCurrentTime += delta;
-                if (disposeCurrentTime > disposeTotalTime)
-                {
-                    disposeCurrentTime = 0;
-                    int curCapacity = (int)(length * 0.5f);
-                    timers.Capacity = curCapacity > minCapacity ? curCapacity : minCapacity;
-                }
-            }
-            else
-            {
-                disposeCurrentTime = 0f;
-            }
-            timerCount = length;
+            //int capacity = timers.Capacity;
+            //if (timerCount < capacity * 0.25f)
+            //{
+            //    disposeCurrentTime += delta;
+            //    if (disposeCurrentTime > disposeTotalTime)
+            //    {
+            //        disposeCurrentTime = 0;
+            //        int curCapacity = (int)(timerCount * 0.5f);
+            //        timers.Capacity = curCapacity > minCapacity ? curCapacity : minCapacity;
+            //    }
+            //}
+            //else
+            //{
+            //    disposeCurrentTime = 0f;
+            //}
         }
 
 
         public static void StartTimer(this ITimer timer)
         {
+            //if (timerCount < timers.Count)
+            //{
+            //    timers[timerCount] = timer;
+            //}
+            //else
+            //{
+            //    timers.Add(timer);
+            //}
+            //timerCount++;
+
             timers.Add(timer);
-            timerCount++;
         }
         public static void StartTimer(this IEnumerable<ITimer> timers)
         {
-            int count = TimerManager.timers.Count;
+
             TimerManager.timers.AddRange(timers);
-            // 增加 相比之前增加的数量
-            timerCount = timerCount + TimerManager.timers.Count - count;
+
+            //TimerManager.timers.RemoveRange();
+
+            //int surplusCount = TimerManager.timers.Count - timerCount;
+            //if (surplusCount > 0)
+            //{
+
+            //}
+
+            //TimerManager.timers.AddRange(timers);
+            //// 增加 相比之前增加的数量
+            //int addCount = TimerManager.timers.Count - count;
+
+            //timerCount = timerCount + ;
         }
     }
 }
