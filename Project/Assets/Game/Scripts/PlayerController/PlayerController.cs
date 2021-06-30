7b1294c4a16e0daed9f7065afb8b8c2d5b13e2b4
cofdream <cofdream@sina.com>
2021-06-30 18:01:00 +0800

    update

--- Project/Assets/Game/Scripts/PlayerController/PlayerController.cs
+++ Project/Assets/Game/Scripts/PlayerController/PlayerController.cs
@@ -14,6 +14,7 @@ namespace Game
         public PlayFiniteStateMachine PlayFiniteStateMachine;
 
         public bool IsJump;
+        public bool IsRun;
         public Movement Movement;
         public Transform ModeTransform;
 
@@ -25,11 +26,33 @@ namespace Game
 
         public void Update()
         {
+            GetInputValues();
+
             PlayFiniteStateMachine.Update(this);
         }
         public void FixedUpdate()
         {
             PlayFiniteStateMachine.FixedUpdate(this);
+        }
+
+        private void GetInputValues()
+        {
+            IsRun = Input.GetAxis("Fire3") == 1;
+            IsJump = Input.GetButtonDown("Jump");
+
+            Movement.Horizontal = Input.GetAxis("Horizontal");
+            Movement.Vertical = Input.GetAxis("Vertical");
+
+            float maxValue;
+            if (IsRun)
+                maxValue = 1;
+            else
+                maxValue = 0.25f;
+
+            var moveAmount = Mathf.Clamp(Mathf.Abs(Movement.Horizontal) + Mathf.Abs(Movement.Vertical), 0, maxValue);
+            Movement.MoveAmount = moveAmount;
+
+            //IsMovement.Value = moveAmount > 0.1f;
         }
     }
 }
